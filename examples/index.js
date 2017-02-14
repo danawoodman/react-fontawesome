b76c186c923da4ec42217eb3f0b98d09355f434a
Dana Woodman <dana@danawoodman.com>
2017-02-14 10:03:00 -0800

    Add stack example.

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,17 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const FA = require('react-fontawesome')
 
+// Spinning
 ReactDOM.render(
   <FA name='twitter' spin />,
-  document.getElementById('root')
+  document.getElementById('spin')
+)
+
+// Stack
+ReactDOM.render(
+  <span className='fa-stack fa-lg'>
+    <FA name='square-o' stack='2x' />
+    <FA name='twitter' stack='1x' />
+  </span>,
+  document.getElementById('stacked')
 )
